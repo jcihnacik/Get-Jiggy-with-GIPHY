b3df5193b169f4749a733b482238e71c310d91f0
jcihnacik <jcihnacik@gmail.com>
2019-09-19 18:16:07 -0400

    working game

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 
 
-var topics = ["clowns", "balloons", "cake", "ice cream", "dancing", "feasting", "blow out candles", "musical chairs", "punchbowl", "pin the tail on the donkey",  "cheers",];
+var topics = ["foxtrot", "cha cha slide", "disco", "breakdance", "waltz", "macarena", "nae nae", "tango", "square dance", "salsa"];
 
 
 
@@ -17,62 +17,75 @@ function renderButtons() {
 renderButtons();
 
 
-$("#add-topic").on("click", function(event){
+$("#add-topic").on("click", function (event) {
     event.preventDefault();
     var newTopic = $("#user-input").val().trim();
-  if(newTopic == ""){
-   alert ("Please add something to the party!")
-  } else 
-    {topics.push(newTopic)
-  $("#button-container").empty();
-  renderButtons();
-  buildQueryURL();}
-    });
+    if (newTopic == "") {
+        alert("Please specify which move you'd like to bust out on the floor!")
+    } else {
+        topics.push(newTopic)
+        $("#button-container").empty();
+        renderButtons();
+    }
+});
 
-function buildQueryURL(){
-  var queryTopic= $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/" + queryTopic + "?api_key=EVTOFSC74nifb3laQy7JCgO9eswgZ6WQ&limit=12";
-   $.ajax({
+
+function getGIPHY() {
+    $("#gifs-appear-here").empty();
+    var queryTopic = $(this).attr("data-name");
+    console.log(queryTopic);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + queryTopic + "&api_key=EVTOFSC74nifb3laQy7JCgO9eswgZ6WQ&limit=10";
+
+    $.ajax({
         url: queryURL,
         method: "GET"
-      })
-        // After the data comes back from the API
-        .then(function(response) {
-          // Storing an array of results in the results variable
-          var results = response.data;
+    })
 
-          // Looping over every result item
-          for (var i = 0; i < results.length; i++) {
+        .then(function (response) {
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].rating !== "r") {
+                    var gifDiv = $("<div>");
+                    var rating = results[i].rating;
+                    var p = $("<p>").text("Rating: " + rating);
+                    var animated = results[i].images.fixed_height.url;
+                    var still = results[i].images.fixed_height_still.url;
 
-            // Only taking action if the photo has an appropriate rating
-            if (results[i].rating !== "r") {
-              // Creating a div for the gif
-              var gifDiv = $("<div>");
-
-              // Storing the result item's rating
-              var rating = results[i].rating;
-
-              // Creating a paragraph tag with the result item's rating
-              var p = $("<p>").text("Rating: " + rating);
-
-              // Creating an image tag
-              var topicImage = $("<img>");
-
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-             topicImage.attr("src", results[i].images.fixed_height.url);
-
-              // Appending the paragraph and personImage we created to the "gifDiv" div we created
-              gifDiv.append(p);
-              gifDiv.append(topicImage);
-
-              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-              $("#gifs-appear-here").prepend(gifDiv);
+                    var topicImage = $("<img>");
+                    topicImage.attr("src", animated);
+                    topicImage.attr("data-still", still);
+                    topicImage.attr("data-animate", animated);
+                    topicImage.attr("class", "gif");
+                    gifDiv.append(p);
+                    gifDiv.append(topicImage);
+                    $("#gifs-appear-here").append(gifDiv);
+                }
             }
-          }
-     $("#user-input").empty();
-  
-   })};
+            $("#user-input").empty();
+
+        })
+};
+
+$(".gif-topic").on("click", getGIPHY);
+
+function startAndStop(){
+    $(".gif").on("click", startAndStop())
+}
+
+$(document).on('click', '.gif', function () {
+    console.log("startandstop called");
+    var state = $(this).attr("data-state");
+    var animateImage = $(this).attr("data-animate");
+    var stillGIF = $(this).attr("data-still");
+
+    if (state == "still") {
+        $(this).attr("src", animateImage);
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", stillGIF);
+        $(this).attr("data-state", "still");
+    }
+});
 
 
 
